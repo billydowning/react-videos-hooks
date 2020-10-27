6b2a6a6393865379d09a8bf10a80a278f7c3c162
Billy Downing <wldowning@gmail.com>
2020-10-27 13:35:55 -0600

    function refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [ videos, setVideos ] = useState([]);
+  const [ selectedVideo, setSelectedVideo ] = useState(null);
 
-  componentDidMount() {
-    this.onTermSubmit('dogs');
-  }
+  useEffect(() => {
+    onTermSubmit('dogs');
+  }, []);
 
-  onTermSubmit = async (term) => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video })
-  };
 
-  render() {
-    return (
+
+  return (
     <div className="ui container">
-      <SearchBar onFormSubmit={this.onTermSubmit} />
+      <SearchBar onFormSubmit={onTermSubmit} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
-            <VideoDetail video={this.state.selectedVideo} />
+            <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
             <VideoList
-              onVideoSelect={this.onVideoSelect}
-              videos={this.state.videos}
+              onVideoSelect={setSelectedVideo}
+              videos={videos}
               />
           </div>
         </div>
       </div>
     </div>
     );
-  }
-}
+};
+
 
 export default App;
